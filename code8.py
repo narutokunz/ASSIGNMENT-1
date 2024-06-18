419aef27cf7ddec4b2d8f57f5634ff93a884dbd3
narutokunz <niharika128btcse23@igdtuw.ac.in>
2024-06-18 21:03:28 +0530

    Create code8.py

---
+++ code8.py
@@ -0,0 +1,11 @@
+def concatenate_strings(str1, str2):
+    concatenated_string = str1 + str2
+    return concatenated_string
+
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+
+result = concatenate_strings(string1, string2)
+
+print(f"The concatenated string is: {result}")
+
