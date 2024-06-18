12b02e56bb8adeb7fbc2a825892ef0a84ad50865
narutokunz <niharika128btcse23@igdtuw.ac.in>
2024-06-18 21:04:40 +0530

    Create code10.py

---
+++ code10.py
@@ -0,0 +1,10 @@
+def convert_to_uppercase(input_string):
+
+    uppercase_string = input_string.upper()
+    return uppercase_string
+
+input_string = input("Enter a string to convert to uppercase: ")
+
+uppercase_string = convert_to_uppercase(input_string)
+
+print(f"The uppercase version of the string is: {uppercase_string}")
